e88c5fe3747dbcc3e859b43e01daf5a08927f55c
Abdul Manaf <sahitoa97@gmail.com>
2022-08-31 00:09:51 +0500

    tricky

--- src/Practice_10.kt
+++ src/Practice_10.kt
@@ -1,2 +1,10 @@
-class Practice_10 {
+fun main(args: Array<String>) {
+    val names = arrayOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    var day = readLine()!!.toInt()
+    if(day < 1 || day > 7){
+        println("Invalid day")
+    }
+    else {
+        println(names[day])
+    }
 }
---
+++ src/Practice_11.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+        var str = readLine()
+        if (str != null) {
+                for(i in str.toCharArray()){
+                        println(i)
+            }
+        }
+}
